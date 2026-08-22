05b96851dd27bb868e7cb41bf818885fd9bc649e
Jesus Reyes <luisdejesus200122@gmail.com>
2026-08-22 09:54:13 -0600

    feat: actualizar método findAll para incluir el usuario y agregar funcionalidad para crear clientes

--- src/modules/clientes/clientes.controller.ts
+++ src/modules/clientes/clientes.controller.ts
@@ -1,17 +1,32 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, ParseIntPipe, Post, Req } from '@nestjs/common';
+import type { Request } from 'express';
 import { ClientesService } from './clientes.service';
+import { CreateClienteDto } from './dto/create-cliente.dto';
 
 @Controller('clientes')
 export class ClientesController {
     constructor(private readonly clientesService: ClientesService) { }
 
     @Get()
-    async findAll() {
-        const clientes = await this.clientesService.findAll();
+    async findAll(@Req() req: Request) {
+        const { userId } = req.user as { userId: number };
+        const clientes = await this.clientesService.findAll(userId);
         return {
             ok: true,
             msg: 'Clientes obtenidos correctamente',
             clientes,
+        };
+    }
+
+    @Post()
+    @HttpCode(201)
+    async create(@Body() dto: CreateClienteDto, @Req() req: Request) {
+        const { userId } = req.user as { userId: number };
+        const cliente = await this.clientesService.create(dto, userId);
+        return {
+            ok: true,
+            msg: 'Cliente creado correctamente',
+            cliente,
         };
     }
 
