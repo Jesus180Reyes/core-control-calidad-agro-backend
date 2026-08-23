0f28a17552c3d8777a4f5c5e41c11a9c4083dd9f
Jesus Reyes <luisdejesus200122@gmail.com>
2026-08-22 23:26:15 -0600

    feat: agregar servicio de lotes con métodos para crear y listar lotes por cliente

---
+++ src/modules/lotes/lotes.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { LotesRepository } from './repository/lotes.repository';
+import { CreateLoteDto } from './dto/create-lote.dto';
+
+@Injectable()
+export class LotesService {
+    constructor(private readonly lotesRepository: LotesRepository) { }
+
+    async findAllByCliente(clienteId: number, userId: number) {
+        return await this.lotesRepository.getLotesByCliente(clienteId, userId);
+    }
+
+    async create(dto: CreateLoteDto, userId: number) {
+        return await this.lotesRepository.createLote(dto, userId);
+    }
+}
