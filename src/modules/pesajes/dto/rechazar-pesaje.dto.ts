a974bea254f87ac66492d612b5ffb818c79b1276
Jesus Reyes <luisdejesus200122@gmail.com>
2026-09-01 12:41:21 -0600

    fix: corregir mensaje de error en la validación del motivo de rechazo

--- src/modules/pesajes/dto/rechazar-pesaje.dto.ts
+++ src/modules/pesajes/dto/rechazar-pesaje.dto.ts
@@ -2,7 +2,7 @@ import { createZodDto } from "nestjs-zod";
 import z from "zod";
 
 const rechazarPesajeSchema = z.object({
-    motivo: z.string({ error: 'Motivo requerido' }).min(5, 'El motivo debe tener al menos 5 caracteres').max(255, 'El motivo no puede exceder los 255 caracteres'),
+    motivo: z.string({ error: 'Motivo requerido' }).min(5, 'El motivo de rechazo debe tener al menos 5 caracteres').max(255, 'El motivo no puede exceder los 255 caracteres'),
 });
 
 export class RechazarPesajeDto extends createZodDto(rechazarPesajeSchema) { }
