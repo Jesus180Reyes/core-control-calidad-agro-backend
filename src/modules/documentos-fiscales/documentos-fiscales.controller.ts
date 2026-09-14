5dfbeb2e15f444f2b923667a17be159c4ef0fb03
Jesus Reyes <luisdejesus200122@gmail.com>
2026-09-14 10:43:22 -0600

    feat: implementar controlador y servicio para la creación de documentos fiscales

--- src/modules/documentos-fiscales/documentos-fiscales.controller.ts
+++ src/modules/documentos-fiscales/documentos-fiscales.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import type { Request } from 'express';
 import { DocumentosFiscalesService } from './documentos-fiscales.service';
+import { CreateDocumentoFiscalDto } from './dto/create-documento-fiscal.dto';
 
 @ApiTags('documentos-fiscales')
 @ApiBearerAuth()
@@ -9,4 +11,19 @@ export class DocumentosFiscalesController {
     constructor(
         private readonly documentosFiscalesService: DocumentosFiscalesService,
     ) { }
+
+    @Post()
+    @HttpCode(201)
+    async create(@Body() dto: CreateDocumentoFiscalDto, @Req() req: Request) {
+        const { userId } = req.user as { userId: number };
+        const documento_id = await this.documentosFiscalesService.create(
+            dto,
+            userId,
+        );
+        return {
+            ok: !!documento_id,
+            msg: 'Documento fiscal registrado correctamente',
+            documento_id,
+        };
+    }
 }
--- src/modules/documentos-fiscales/documentos-fiscales.service.ts
+++ src/modules/documentos-fiscales/documentos-fiscales.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { DocumentosFiscalesRepository } from './repository/documentos-fiscales.repository';
+import { CreateDocumentoFiscalDto } from './dto/create-documento-fiscal.dto';
 
 @Injectable()
-export class DocumentosFiscalesService { }
+export class DocumentosFiscalesService {
+    constructor(
+        private readonly documentosFiscalesRepository: DocumentosFiscalesRepository,
+    ) { }
+
+    async create(dto: CreateDocumentoFiscalDto, userId: number) {
+        return await this.documentosFiscalesRepository.createDocumentoFiscal(
+            dto,
+            userId,
+        );
+    }
+}
